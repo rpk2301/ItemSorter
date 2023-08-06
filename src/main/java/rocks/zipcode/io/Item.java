952ab1fdcf8a9ef3ae71563d8cb72d6a7f02c03d
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-08-05 21:41:37 -0400

    Finished

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,51 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
-    public Item() {
+
+    Long id;
+    String name;
+    Double price;
+    public Item()
+    {
+        this.name = "";
+        this.id = 0L;
+        this.price = 0.01;
     }
 
-    public Item(Long id, String name, Double price) {
+    public Item(Long id, String name, Double price)
+    {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
-    public String getName() {
-        return null;
+    public String getName()
+    {
+        return this.name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
+        this.name = name;
     }
 
-    public Long getId() {
-        return null;
+    public Long getId()
+    {
+        return this.id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id)
+    {
+        this.id = id;
     }
 
-    public Double getPrice() {
-        return null;
+    public Double getPrice()
+    {
+        return this.price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(Double price)
+    {
+        this.price = price;
     }
 }
--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,23 @@
 package rocks.zipcode.io;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
     public ItemSorter(Item[] items) {
+    this.items = items;
     }
 
-    public Item[] sort(Comparator<Item> comparator) {
-        return null;
+    public Item[] sort(Comparator<Item> comparator)
+    {
+        ArrayList<Item> unsorted = new ArrayList<Item>(Arrays.asList(items));
+        unsorted.sort(comparator);
+        return unsorted.toArray(new Item[items.length]);
     }
 }
